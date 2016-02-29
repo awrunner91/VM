5c83d4e742567a94f9542bffc9c669204c1e6a4b
Anthony Woods <Tony@Anthonys-MacBook-Pro.local>
2016-02-28 19:48:09 -0800

    Added more instructions

--- src/parseFuncs.c
+++ src/parseFuncs.c
@@ -9,6 +9,14 @@
 #include <string.h>
 #include <math.h>
 
+#define NUMCODES 11				//Number of instructions
+#define NUMREGS 16				//Number of registers
+#define SAME 0					//Used to strncmp
+#define ERROR -1				//Used if invalid instruction/register
+
+/* Define possible instructions. */
+char *instructs[] = {"HALT","LOAD","ADD","SUB","MULT","DIV","AND","XOR","JUMP","CJMP","INC"};
+
 /**
  * Function to get the minimum of two integers.
  * a: First argument.
@@ -35,19 +43,15 @@ int get_min(int a,int b)
 **/
 int get_instr_code(char *instr)
 {
-	int SAME = 0;				//Used for str comparisons.
-	int ERROR = -1;				//Used if instruction not found.
-	int NUMCODES = 3;			//Number of instructions/codes. CHANGE!!!!
 	int instr_size = strlen(instr);		//Length of argument string.
 	int MAXCHARS;				//Will hold max number of chars for strncmp.
-	char *codes[] = {"HALT","LOAD","ADD"};	//Array of instructions.
 
 	/* Iterate and find index for instruction */
 	for (int codeNum = 0; codeNum < NUMCODES; codeNum++) 
 	{
-		MAXCHARS = get_min(instr_size,strlen(codes[codeNum]));	
+		MAXCHARS = get_min(instr_size,strlen(instructs[codeNum]));
 	
-		if (strncmp(instr,codes[codeNum],MAXCHARS) == SAME) 
+		if (strncmp(instr,instructs[codeNum],MAXCHARS) == SAME) 
 		{
 			return codeNum;
 		}
@@ -60,34 +64,33 @@ int get_instr_code(char *instr)
  * Function to get the register code from
  * a string.
  * reg: Pointer to register string (R0,R1,..R15).
- * return: Register code (integer from 0-15).
+ * return: Register code (integer from 0-15). Long because using strtol.
 **/
-int get_reg_code(char *reg)
+long int get_reg_code(char *reg)
 {
-	int SAME = 0;				//Used for str comparisons.
-	int ERROR = -1;				//Used if register number is invalid.
-	int NUMREGS = 4;			//Number of possible registers. CHANGE!!!!
-	int MAXCHARS;				//Will hold max number of chars for strncmp.
-	int reg_size = strlen(reg);		//Length of argument string.
-	char *regs[] = {"R0","R1","R2","R3"};	//Array of registers.
-
+	long reg_code;				//Will hold register code.
+	char *ptr;				//Used with strtol.
+	int BASE = 10;				//Used with strtol.
+	
 	/* Deal with empty case. */
 	if (reg[0] == '\0') {
 		return 0;
 	}
-
-	/* Iterate and find index of register. */
-	for (int regNum = 0; regNum < NUMREGS; regNum++) 
+	else
 	{
-		MAXCHARS = get_min(reg_size,strlen(regs[regNum]));
-
-		if (strncmp(reg,regs[regNum],MAXCHARS) == SAME)
-		{
-			return regNum;
-		}
+		/* Grab the reg num - starts at index 1. */
+		reg_code = strtol(&reg[1],&ptr,BASE);	
 	}
 
-	return ERROR;
+	/* Error check. */
+	if (reg_code < 0 || reg_code > 15)
+	{
+		return ERROR;
+	}
+	else
+	{
+		return reg_code;
+	}	
 }
 
 /**
@@ -127,90 +130,36 @@ long int get_const(char *num_str)
 **/
 void create_opcode_arr(char *input_str,int op_codes[])
 {
-	/* Constants for initializing arrays. */
-	const int MAXINSTRSIZE = 4;			//Max instruction length
-	const int MAXREGSIZE = 3;			//Max register length
-	const int MAXCONSTSIZE = 4;			//Max const number length
+	/* Char array sizes for parsing. */
+	int MAXINSTRSIZE = 4;			//Max instruction length. 
+	int MAXREGSIZE = 3;			//Max register length.
+	int MAXCNSTSIZE = 4;			//Max constant length.
+
+	/* Char arrays for parsed input. */	
+	char instr[MAXINSTRSIZE];		//Will hold instruction.
+	char dest_reg[MAXREGSIZE];		//Will hold destination register.
+	char arg_reg1[MAXREGSIZE];		//Will hold argument 1 register.
+	char arg_reg2[MAXREGSIZE];		//Will hold argument 2 register.
+	char cnst_num[MAXCNSTSIZE];		//Will hold the constant.
+
+	/* Numbers for op-codes. */
+	int instr_code;				//Op-code for instruction.
+	int dest_reg_code;			//Op-code for destination register.
+	int arg_reg1_code;			//Op-code for argument 1 register.
+	int arg_reg2_code;			//Op-code for argument 2 register.
+	int cnst_code;				//Op-code for constant number.
+
+	/* Misc. */
+	char *token;				//Token for parsing the input.
+
+	/* Get the instruction op-code. */
+	token = strtok(input_str," ");		//Assume space separated.
+	strncpy(instr,token,strlen(token));
+	instr_code = get_instr_code(instr);
+
 	
-	/* Arrays to hold tokens. */
-	char instr[MAXINSTRSIZE];			//Holds instruction
-	char dest_reg[MAXREGSIZE];			//Holds destination register
-	char arg_reg1[MAXREGSIZE];			//Holds argument register 1
-	char arg_reg2[MAXREGSIZE];			//Holds argument register 2
-	char cnst_num[MAXCONSTSIZE];			//Holds constant number from input
-
-	/* Op-code variables. */
-	int instr_code;					//Holds instruction code
-	int dest_reg_code;				//Holds destination register code
-	int arg_reg1_code;				//Holds argument register 1 code
-	int arg_reg2_code;				//Holds argument register 2 code
-	int cnst_num_code;				//Holds the constant number from instr
+	return;
 	
-	/* Misc variables. */
-	char *token;					//Will hold tokens from parsing input.
-	int SAME = 0;					//Used for string comparison.
-	int cmp_size;					//Used in strncmp.
-	int cpy_size;					//Used in strncpy.
-
-	/* Get instruction. */
-	token = strtok(input_str," ");
-	strncpy(instr,token,MAXINSTRSIZE);
-	token = strtok(NULL," ");
-
-	/* If HALT, other set op-codes to 0. */
-	cmp_size = get_min(strlen(instr),strlen("HALT"));
-	if (strncmp(instr,"HALT",cmp_size) == SAME)
-	{
-		instr_code = 0;
-		dest_reg_code = 0;
-		arg_reg1_code = 0;
-		arg_reg2_code = 0;
-		cnst_num_code = 0;
-	}
-
-	/* Not HALT, so get other codes. */	
-	else
-	{	
-		strncpy(dest_reg,token,MAXREGSIZE);
-		token = strtok(NULL," ");
-
-		/* If no arg registers, get cnst_num. */
-		if (strstr(token,"#") != NULL)
-		{
-			strncpy(cnst_num,token,MAXCONSTSIZE);
-			printf("CNST: %s\n",cnst_num);
-			arg_reg1[0] = '\0';
-			arg_reg2[0] = '\0';
-		}
-
-		/* Grab arg registers if no cnst_num. */
-		else
-		{
-			strncpy(arg_reg1,token,MAXREGSIZE);
-			printf("REG 1 token: %s\n",token);
-			token = strtok(NULL," ");
-			printf("REG 2 token: %s\n",token);
-			strncpy(arg_reg2,token,MAXREGSIZE);
-			cnst_num[0] = '\0';
-		}
-
-		/* Generate op-codes and put in op_codes. */
-		instr_code = get_instr_code(instr);
-		dest_reg_code = get_reg_code(dest_reg);
-		arg_reg1_code = get_reg_code(arg_reg1);
-		arg_reg2_code = get_reg_code(arg_reg2);
-		cnst_num_code = get_const(cnst_num);
-
-	}
-
-	/* Set all vals in op_code arr. */
-	op_codes[0] = cnst_num_code;
-	op_codes[1] = arg_reg2_code;
-	op_codes[2] = arg_reg1_code;
-	op_codes[3] = dest_reg_code;
-	op_codes[4] = instr_code;
-
-	return;	
 }
 
 
